3c8059f2bfdfeb305d2548bd0fc1413cbe5464a3
Cynthia McMahohn <cynth.mcmahon@gmail.com>
2020-11-25 18:27:20 -0500

    Tutoria 01

---
+++ ForestRush/Psilibrary/StateManager.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Psilibrary.StateManager
+{
+    public interface IStateManager
+    {
+        GameState CurrentState { get; }
+
+        event EventHandler StateChanged;
+
+        void PushState(GameState state);
+        void ChangeState(GameState state);
+        void PopState();
+        bool ContainsState(GameState state);
+    }
+
+    public class StateManager : GameComponent, IStateManager
+    {
+        #region Field Region
+
+        private readonly Stack<GameState> gameStates = new Stack<GameState>();
+
+        private const int startDrawOrder = 5000;
+        private const int drawOrderInc = 50;
+        private int drawOrder;
+
+        #endregion
+
+        #region Event Handler Region
+
+        public event EventHandler StateChanged;
+
+        #endregion
+
+        #region Property Region
+
+        public GameState CurrentState
+        {
+            get { return gameStates.Peek(); }
+        }
+
+        #endregion
+
+        #region Constructor Region
+
+        public StateManager(Game game)
+            : base(game)
+        {
+            Game.Services.AddService(typeof(IStateManager), this);
+        }
+
+        #endregion
+
+        #region Method Region
+
+        public void PushState(GameState state)
+        {
+            AddState(state);
+            OnStateChanged();
+        }
+
+        private void AddState(GameState state)
+        {
+            drawOrder += drawOrderInc;
+            state.DrawOrder = drawOrder;
+
+            gameStates.Push(state);
+            Game.Components.Add(state);
+            StateChanged += state.StateChanged;
+        }
+
+        public void PopState()
+        {
+            if (gameStates.Count != 0)
+            {
+                RemoveState();
+                OnStateChanged();
+            }
+        }
+
+        private void RemoveState()
+        {
+            GameState state = gameStates.Peek();
+            drawOrder -= drawOrderInc;
+            StateChanged -= state.StateChanged;
+            Game.Components.Remove(state);
+            gameStates.Pop();
+        }
+
+        public void ChangeState(GameState state)
+        {
+            while (gameStates.Count > 0)
+                RemoveState();
+
+            drawOrder = startDrawOrder;
+            state.DrawOrder = drawOrder;
+            drawOrder += drawOrderInc;
+
+            AddState(state);
+            OnStateChanged();
+        }
+
+        public bool ContainsState(GameState state)
+        {
+            return gameStates.Contains(state);
+        }
+
+        protected internal virtual void OnStateChanged()
+        {
+            StateChanged?.Invoke(this, null);
+        }
+
+        #endregion
+    }
+}
